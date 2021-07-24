11995f2d94ea8fd87f8ea5579619c9acd63337bd
Joel Bello <joel8bl@gmail.com>
2021-07-24 01:08:35 -0400

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My Super Cool Blog",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
-      }
+        path: `${__dirname}/blog/`,
+      },
     },
+    "gatsby-plugin-mdx",
   ],
 };
