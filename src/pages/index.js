6d44ca246bba23e787e7c9540997987dd2981d03
Joel Bello <joel8bl@gmail.com>
2021-07-24 00:49:38 -0400

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import * as React from "react";
-import { Link } from "gatsby";
-import Layout from "../components/layout";
-
-export const IndexPage = () => {
+import * as React from 'react'
+import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Developer"
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=80"
+      />
     </Layout>
-  );
-};
-
-export default IndexPage;
+  )
+}
+export default IndexPage
